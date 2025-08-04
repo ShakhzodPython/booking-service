9b1d7fa39f835a3ae5d6e47a77a767a3fcb393d1
Shakhzod Komilov <komilovshazod57@gmail.com>
2025-08-04 17:19:06 +0500

    updated import of models

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -2,10 +2,21 @@ __all__ = (
     "db_helper",
     "Base",
     "Place",
-    "PlaceLocation",
+    "PlaceType",
+    "PlaceDetail",
     "PlaceService",
+    "PlaceLocation",
+    "PlaceServiceAssociation",
 )
 
 from .db_helper import db_helper
 from .base import Base
-from .place import Place, PlaceLocation, PlaceService
+from .place import (
+    Place,
+    PlaceType,
+    PlaceDetail,
+    PlaceService,
+    PlaceLocation,
+
+)
+from .place_service_association import PlaceServiceAssociation
